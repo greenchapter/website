0d363f6e5d8e09080c9e390f981d732cbfc1a3a4
Thomas Ott <thomas.ott@greenchapter.de>
2023-05-03 14:11:10 +0200

    feat(design): add the first new design version

--- src/routes/Component.svelte
+++ src/routes/Component.svelte
@@ -6,9 +6,10 @@
 </div>
 
 <section class="intro">
-	<h1>Those who can imagine anything, can create the impossible.</h1>
-
-	<span>— Alan Turing</span>
+	<h1>
+		Director of Technology at Kaiser X Labs and founder of Pace Engineering. <img src="memoji.png" width="75px" /><br />
+		I develop and design the most engaging contemporary digital product experiences.
+	</h1>
 </section>
 <style>
 	.component {
@@ -21,20 +22,27 @@
 
 	@media only screen and (max-width: 600px) {
 		.component {
-			font-size: 1.5rem;
+			font-size: 1.25rem;
 			line-height: 1.75rem;
 		}
 	}
 
 	h1 {
-		font-style: normal;
-		font-weight: 500;
-		font-size: 2.8125rem;
-		line-height: 100%;
-		color: #e4e4e4;
 		margin-top: 8rem;
 		margin-bottom: 1.8rem;
-		width: 700px;
+		font-family: 'Inter Display V';
+		font-style: normal;
+		font-weight: 400;
+		font-size: 2.625rem;
+		line-height: 115%;
+		width: 925px;
+		height: 219px;
+	}
+
+	h1 img {
+		top: 8px;
+    	left: 2px;
+    	position: relative;
 	}
 
 	@media only screen and (max-width: 600px) {
@@ -44,8 +52,7 @@
 			width: 100%;
 		}
 	}
-
-	.intro span {
-		color: rgba(49, 49, 49, 0.5);
-	}
 </style>
+
+
+
